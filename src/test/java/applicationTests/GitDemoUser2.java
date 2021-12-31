c0a13a3fcafb3f198fab1bcdcd455c5c8c272553
Santosh <thetechgig@gmail.com>
2021-12-31 12:50:32 +0530

    Architect of DemoUse2

---
+++ src/test/java/applicationTests/GitDemoUser2.java
@@ -0,0 +1,15 @@
+package applicationTests;
+
+public class GitDemoUser2 {
+
+	public void newDevelopment()
+	{
+		System.out.println("Changes made by DemoUser2");
+	}
+	
+	public void newTest()
+	{
+		System.out.println("updated by Demouser2");
+	}
+	
+}
